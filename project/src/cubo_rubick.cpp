2e7ef2e6647740a840e2057b6534f3062d989ab4
gretaSchiavi <greta.schiavi2005@gmail.com>
2023-05-16 21:50:43 +0200

    Aggiunta valori colore paragrafi

--- project/src/cubo_rubick.cpp
+++ project/src/cubo_rubick.cpp
@@ -9,18 +9,18 @@ int main()
 {
 	//
 	doc::title("Cubo di Rubik");
-	doc::subtitle("Progetto della 4E, sulla realizzazione di un programma finalizzato alla risoluzione di un cubo di Rubik.");
+	doc::subtitle("Progetto della 4E, sulla realizzazione di un programma finalizzato alla risoluzione di un cubo di Rubik. \n");
 	doc::paragraph("STORIA \n", 202);
-	doc::paragraph("Nel 1974 Erno Rubik, insegnante del dipartimento di ''Interior Design'' della ''Moholy-Nagy university \n");
-	doc::paragraph("of Art and Design di Budapest'', creò un cubo che permettesse di muovere ogni faccia in modo indipendente. \n");
-	doc::paragraph("Solo dopo aver creato il cubo e averlo scomposto, non riuscendo più a ricomporlo, Rubik si rese conto di aver  \n");
-	doc::paragraph("creato un vero e proprio rompicapo. \n");
-	doc::paragraph("Originariamente il cubo prende il nome di ''cubo magico'', il nome attuale gli venne conferito solo nel 1980\n");
-	doc::paragraph("dall'Ideal Toy, un'azienda di giochi statunitense.\n");
-	doc::paragraph("Nel 1975 Erno Rubik ottenne il brevetto per il cubo magico, riuscendo così a distribuirlo nei maggiori\n");
-	doc::paragraph("negozi di giocattoli, riscuotendo un enorme successo.\n");
-	doc::paragraph("Stime affermano che tra il 1980 e il 1983 siano stati venduti circa 200 milioni di cubi in tutto il mondo,\n");
-	doc::paragraph(" grazie anche alle fiere di giocattoli in Europa e negli Stati Uniti.\n");
+	doc::paragraph("Nel 1974 Erno Rubik, insegnante del dipartimento di ''Interior Design'' della ''Moholy-Nagy university \n", 48);
+	doc::paragraph("of Art and Design di Budapest'', creò un cubo che permettesse di muovere ogni faccia in modo indipendente. \n", 38);
+	doc::paragraph("Solo dopo aver creato il cubo e averlo scomposto, non riuscendo più a ricomporlo, Rubik si rese conto di aver  \n", 53);
+	doc::paragraph("creato un vero e proprio rompicapo. \n",228);
+	doc::paragraph("Originariamente il cubo prende il nome di ''cubo magico'', il nome attuale gli venne conferito solo nel 1980\n", 74);
+	doc::paragraph("dall'Ideal Toy, un'azienda di giochi statunitense.\n", 110);
+	doc::paragraph("Nel 1975 Erno Rubik ottenne il brevetto per il cubo magico, riuscendo così a distribuirlo nei maggiori\n", 82);
+	doc::paragraph("negozi di giocattoli, riscuotendo un enorme successo.\n", 89);
+	doc::paragraph("Stime affermano che tra il 1980 e il 1983 siano stati venduti circa 200 milioni di cubi in tutto il mondo,\n", 123);
+	doc::paragraph("grazie anche alle fiere di giocattoli in Europa e negli Stati Uniti.\n", 125);
 	srand(time(nullptr));
 
 
--- project/src/librerie/documentazione/documentation.cpp
+++ project/src/librerie/documentazione/documentation.cpp
@@ -12,13 +12,13 @@ namespace doc {
 	void subtitle(std::string subtitle) {
 		std::string final_str = "";
 							/*testo, sfondo*/
-		final_str += "\033[38;5;173;48;5;232m" + subtitle + "\033[m" + '\n';
+		final_str += "\033[38;5;15;48;5;211m" + subtitle + "\033[m" + '\n';
 		std::cout << final_str;
 	}
 	void paragraph(std::string paragraph, int color_num) {
 		std::string final_str = "";
 		std::string color = std::to_string(color_num);
-		final_str += "\033[38; 5;" + color + "; 48; 5; 232m" + paragraph + "\033[m" + '\n';
+		final_str += "\033[38; 5;" + color + "; 48; 5; 211m" + paragraph + "\033[m" + '\n';
 		std::cout << final_str; 
 	}
 	
