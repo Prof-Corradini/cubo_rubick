11f27772aff93792c5ff5580e3fa44b4da30d510
/\lecor <alessio.corradini98@gmail.com>
2023-05-17 17:40:22 +0200

    Risolto il problema della visualizzazione dei colori, riportate le funzioni di abbellimento del testo in documentazione
    Il problem dei colori si presentava perché la stringa era scritta nel seguente modo:
    final_str += "\033[38; 5;" + color + "; 48; 5; 211m" + paragraph + "\033[m" + '\n'
    cioè con degli spazi fra i caratteri delle seguenti stringhe:
    "; 5" e  "; 48; 5; 211m"
    Ho rimosso gli spazi

--- project/src/librerie/documentazione/documentation.cpp
+++ project/src/librerie/documentazione/documentation.cpp
@@ -6,7 +6,7 @@
 namespace doc {
 	void title(std::string title) {
 		std::string final_str = "";
-		final_str += graph::addFrame(title) + '\n';
+		final_str += addFrame(title) + '\n';
 		std::cout << final_str;
 	}
 	void subtitle(std::string subtitle) {
@@ -18,13 +18,34 @@ namespace doc {
 	void paragraph(std::string paragraph, int color_num) {
 		std::string final_str = "";
 		std::string color = std::to_string(color_num);
-		final_str += "\033[38; 5;" + color + "; 48; 5; 211m" + paragraph + "\033[m" + '\n';
+		final_str += "\033[38;5;" + color + ";48;5;211m" + paragraph + "\033[m" + '\n';
 		std::cout << final_str; 
 	}
 	
 
+	/* Aggiunge una cornice alla stringa passata*/
+	std::string addFrame(std::string str) {
+		std::string final_str = "";
+
+		final_str += '+' + repeat("-", str.size() + 2) + '+';
+		final_str += "\n";
+
+		final_str += "| " + str + " |";
+		final_str += "\n";
+
+		final_str += '+' + repeat("-", str.size() + 2) + '+';
 
 
+		return final_str;
+	}
+
+	std::string repeat(std::string str, int n_times) {
+		std::string final_str = "";
+		for (int i = 0; i < n_times; i++) {
+			final_str += str;
+		}
+		return final_str;
+	}
 
 
 		
--- project/src/librerie/documentazione/documentation.h
+++ project/src/librerie/documentazione/documentation.h
@@ -4,10 +4,13 @@
 #include "../cubo/cube.h"
 #include <string>
 
+
 namespace doc {
 	void title(std::string);
 	void subtitle(std::string);
 	void paragraph(std::string paragraph, int color_num);
+	std::string addFrame(std::string str);
+	std::string repeat(std::string str, int n_times);
 
 	void mainMenu();
 }
