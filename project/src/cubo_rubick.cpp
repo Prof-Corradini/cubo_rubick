cddd52b31cb402372daeb086d5ca14d4a04e52bb
Francesco Baldoni <baldonifrancesco8@gmail.com>
2023-05-20 10:55:48 +0200

    Aggiunta pattern visualizzazione delle facce

--- project/src/cubo_rubick.cpp
+++ project/src/cubo_rubick.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <windows.h>
 #include "librerie/grafica/graphics.h"
 #include "librerie/algoritmi/algorithms.h"
 #include "librerie/documentazione/documentation.h"
@@ -7,7 +8,11 @@
 
 int main()
 {
-	//
+	HWND console = GetConsoleWindow();
+	RECT r;
+	GetWindowRect(console, &r); //stores the console's current dimensions
+	MoveWindow(console, r.left, r.top, 1700, 1000, TRUE); // 800 width, 100 height
+
 	doc::title("Cubo di Rubik");
 	doc::subtitle("Programma sulla simulazione e risoluzione di un cubo di Rubick");
 	srand(time(nullptr));
@@ -22,16 +27,24 @@ int main()
 	Face& rossa		= cubo.getFace(Back);
 
 
-	//std::cout << arancione;
-	//std::cout << verde;
-	//std::cout << bianca;
-	//std::cout << blu;
-	//std::cout << gialla;
-	//std::cout << rossa;
+	std::cout << arancione;
+	std::cout << verde;
+	std::cout << bianca;
+	std::cout << blu;
+	std::cout << gialla;
+	std::cout << rossa;
 	
+	/*
 	std::cout << graph::visualizeFaceRow(bianca);
-	opr::roundFace(bianca);
+	std::cout << graph::visualizeFaceRow(arancione);
+	std::cout << graph::visualizeFaceRow(blu);
+	std::cout << graph::visualizeFaceRow(verde);
+	std::cout << graph::visualizeFaceRow(rossa);
+	std::cout << graph::visualizeFaceRow(gialla);
+	*/
 
+	opr::roundFace(bianca);
+	graph::pattern();
 	alg::stirCube(cubo);
 
 
